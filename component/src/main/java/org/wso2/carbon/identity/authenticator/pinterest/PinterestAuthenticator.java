92a1d3e50d4e2b8633ea67ff45e508a4205ba12f
Asha Sulaiman <165079T@uom.lk>
2023-01-03 17:14:21 +0530

    fix typo
    Co-authored-by: Wathsara Wishwantha Daluwatta <31571237+Wathsara@users.noreply.github.com>

--- component/src/main/java/org/wso2/carbon/identity/authenticator/pinterest/PinterestAuthenticator.java
+++ component/src/main/java/org/wso2/carbon/identity/authenticator/pinterest/PinterestAuthenticator.java
@@ -174,7 +174,7 @@ public class PinterestAuthenticator extends OpenIDConnectAuthenticator implement
 	 * @param request  the http request
 	 * @param response the http response
 	 * @param context  the authentication context
-	 * @throws AuthenticationFailedException if the operation failed.
+	 * @throws AuthenticationFailedException If the operation failed.
 	 */
 	@Override
 	protected void initiateAuthenticationRequest(HttpServletRequest request, HttpServletResponse response,
